cb09a4cc944778fc630a33ad5f5ffccf61bf5b6f
Yassine <boudouriyassine@gmail.com>
2024-12-15 12:34:06 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='switchai',
-    version='0.1.2',
+    version='0.1.post2',
     description='A unified library for interacting with various AI APIs through a standardized interface.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
@@ -12,6 +12,11 @@ setup(
     license="Apache 2.0 License",
     package_dir={"": "src"},
     packages=find_packages("src"),
-    install_requires=[],
+    install_requires=[
+        "openai",
+        "mistralai",
+        "anthropic",
+        "google-generativeai"
+    ],
     python_requires='>=3.6',
 )
