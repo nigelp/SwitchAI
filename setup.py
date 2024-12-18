570d3c6bfd936ad44febe5a1c2233829a0132630
Yassine <boudouriyassine@gmail.com>
2024-12-18 23:17:43 +0100

    Bumped version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='switchai',
-    version='0.1.3',
+    version='0.1.4',
     description='A unified library for interacting with various AI APIs through a standardized interface.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
