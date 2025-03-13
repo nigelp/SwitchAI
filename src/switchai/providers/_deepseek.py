e8125174e1a2b72c57b81313336b9b00ae779991
Yassine <boudouriyassine@gmail.com>
2025-03-13 11:49:47 +0100

    Added support for DeepSeek

---
+++ src/switchai/providers/_deepseek.py
@@ -0,0 +1,17 @@
+from openai import OpenAI
+
+from ._openai import OpenaiClientAdapter
+from ..utils import Task
+
+SUPPORTED_MODELS = {
+    "deepseek-chat": [Task.TEXT_GENERATION],
+    "deepseek-reasoner": [Task.TEXT_GENERATION],
+}
+
+API_KEY_NAMING = "DEEPSEEK_API_KEY"
+
+
+class DeepseekClientAdapter(OpenaiClientAdapter):
+    def __init__(self, model_name: str, api_key: str):
+        self.model_name = model_name
+        self.client = OpenAI(api_key=api_key, base_url="https://api.deepseek.com")
